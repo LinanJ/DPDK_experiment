c899eedc3a96bc859d9b477c439d2b3beedcdb27
LinanJing <41092368+LinanJ@users.noreply.github.com>
2018-11-07 09:49:07 +0800

    Add files via upload
    dpdk安装脚本，debug编译版，无编译优化，安装位置：/usr/src ，安装版本：dpdk-stable-17.11.1

---
+++ dpdk_install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+cd /usr/src/
+wget http://fast.dpdk.org/rel/dpdk-17.11.1.tar.xz
+tar xf dpdk-17.11.1.tar.xz
+export DPDK_DIR=/usr/src/dpdk-stable-17.11.1
+cd $DPDK_DIR
+sed -i "s/CONFIG_RTE_BUILD_SHARED_LIB=n/CONFIG_RTE_BUILD_SHARED_LIB=y/g" config/common_base
+export DPDK_TARGET=x86_64-native-linuxapp-gcc
+export DPDK_BUILD=$DPDK_DIR/$DPDK_TARGET
+export EXTRA_CFLAGS="-O0 -g"
+sed -i "s/CONFIG_RTE_LIBRTE_PMD_AF_PACKET=n/CONFIG_RTE_LIBRTE_PMD_AF_PACKET=y/g" config/common_linuxapp
+make install T=$DPDK_TARGET DESTDIR=install
