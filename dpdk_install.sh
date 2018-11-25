e7cda8589696026d2fbd4ee302aee687ff3ef4d6
LinanJing <41092368+LinanJ@users.noreply.github.com>
2018-11-25 12:06:48 +0800

    Update dpdk_install.sh

--- dpdk_install.sh
+++ dpdk_install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 cd /usr/src/
-wget http://fast.dpdk.org/rel/dpdk-17.11.1.tar.xz
-tar xf dpdk-17.11.1.tar.xz
-export DPDK_DIR=/usr/src/dpdk-stable-17.11.1
+wget http://fast.dpdk.org/rel/dpdk-17.11.4.tar.xz
+tar xf dpdk-17.11.4.tar.xz
+export DPDK_DIR=/usr/src/dpdk-stable-17.11.4
 cd $DPDK_DIR
 sed -i "s/CONFIG_RTE_BUILD_SHARED_LIB=n/CONFIG_RTE_BUILD_SHARED_LIB=y/g" config/common_base
+sed -ri 's,(PMD_PCAP=).*,\1y,' build/.config
 export DPDK_TARGET=x86_64-native-linuxapp-gcc
 export DPDK_BUILD=$DPDK_DIR/$DPDK_TARGET
 export EXTRA_CFLAGS="-O0 -g"
