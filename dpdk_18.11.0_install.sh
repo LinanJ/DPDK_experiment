39f6082b1ec745e0c8f0c714b5236a9dc900f43a
LinanJing <41092368+LinanJ@users.noreply.github.com>
2018-12-11 09:49:02 +0800

    Update dpdk_18.11.0_install.sh

--- dpdk_18.11.0_install.sh
+++ dpdk_18.11.0_install.sh
@@ -6,4 +6,7 @@ cd dpdk-18.11
 make config T=x86_64-native-linuxapp-gcc
 sed -ri 's,(PMD_PCAP=).*,\1y,' build/.config
 make
+mkdir -p /mnt/huge
+mount -t hugetlbfs nodev /mnt/huge
+echo 64 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
 make -C examples RTE_SDK=$(pwd) RTE_TARGET=build O=$(pwd)/build/examples
